08d060b32f238164e1217e6ed15fe284f87db85c
christoforoschari <christoforos_chari@hotmail.com>
2022-05-11 12:10:04 +0300

    sumofRow fuction

--- Lab4.partb/Matrix.cpp
+++ Lab4.partb/Matrix.cpp
@@ -28,3 +28,19 @@ void printMatrix(const double mat[][MAX_COL], const int maxRow) {
 
     }
 }
+
+/** Fuction <code>sumOfRow</code>show the sum of the row.
+*<BR>
+*@param mat the matrix.
+*@param row the number of row.
+*@param maxRow the number of rows in the matrix.
+*/
+double sumOfRow(const double mat[][MAX_COL], const int row, const int maxRow) {
+    assert(row >= 0 && row < maxRow);
+    double sum = 0.0;
+    for (int i = 0; i < maxRow; i++) {
+        sum += mat[row][i];
+    }
+    cout << "Sum of Row = " << sum;
+    return sum;
+}

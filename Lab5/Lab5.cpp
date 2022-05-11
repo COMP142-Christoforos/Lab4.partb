e2d9a69e689d0f9ee4b36cda8d1fc93fa1e442bd
christoforoschari <christoforos_chari@hotmail.com>
2022-05-11 13:25:15 +0300

    Tests methods

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -9,10 +9,59 @@ namespace Lab5
 	TEST_CLASS(Lab5)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
 
+		TEST_METHOD(TestMethodForSumOfRow)
+		{
+			// double sumOfRow(const double mat[][MAX_COL], const int row, const int maxRow)
+
+			// Arrange
+			const int MAX_ROW = 5;
+			double matrix[MAX_ROW][MAX_COL] = { 1, 1, 1 };
+
+			// Act
+			double sum1 = sumOfRow(matrix, 0, MAX_ROW);
+			double sum0 = sumOfRow(matrix, 1, MAX_ROW);
+
+			// Assert
+			Assert::AreEqual(3.0, sum1);
+			Assert::AreEqual(0.0, sum0);
+		}
+
+		//used to Test
+		TEST_METHOD(TestMethodForfillWithRandomNum)
+		{
+			// void fillWithRandomNum(double mat[][MAX_COL], const int maxRow)
+
+			// Arrange
+			const int MAX_ROW = 5;
+			double matrix[MAX_ROW][MAX_COL] = { 1, 2, 0 };
+			double sum2, sum3;
+
+			// Act
+			fillWithRandomNum(matrix, MAX_ROW);
+			sum2 = sumOfRow(matrix, 0, MAX_ROW);
+			sum3 = sumOfRow(matrix, 1, MAX_ROW);
+
+			// Assert
+			Assert::AreNotEqual(3.0, sum3);
+			Assert::AreNotEqual(0.0, sum2);
+		}
+	
+		TEST_METHOD(TestMethodFormakeIdentityMatrix)
+		{
+			// bool makeIdentityMatrix(double mat[][MAX_COL], const int maxRow)
+
+			// Arrange
+			const int MAX_ROW = 5;
+			double matrix[MAX_ROW][MAX_COL] = { 0 };
+			bool apa;
+
+			// Act
+			apa = makeIdentityMatrix(matrix, MAX_ROW);
+
+			// Assert
+			Assert::IsFalse(3.0);
 		}
 	};
+
 }

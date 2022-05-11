2cfa51e22aaf712933d450b00da2f3142fc74964
christoforoschari <christoforos_chari@hotmail.com>
2022-05-11 12:07:49 +0300

    print matrix fuctio

---
+++ Lab4.partb/Matrix.cpp
@@ -0,0 +1,30 @@
+/** \file Matrix.cpp
+*\bried Headers file from small mattrix library
+*\Athur Christoforos Chari
+*\Date 08/04/2022
+*/
+#include"Matrix.h"
+#include<iostream>
+#include<cassert>
+
+using namespace std;
+
+
+
+
+/** Fuction <code>printMatrix</code>show the content of the matrix
+*<BR>
+*@param mat the matrix
+*@param maxRow the number of rows in the matrix
+*/
+void printMatrix(const double mat[][MAX_COL], const int maxRow) {
+
+    assert(maxRow > 0);
+    for (int i = 0; i < maxRow; i++) {
+        for (int j = 0; j < MAX_COL; j++) {
+
+            cout << mat[i][j] << "\t";
+        }
+
+    }
+}
--- Lab4.partb/Matrix.h
+++ Lab4.partb/Matrix.h
@@ -7,6 +7,7 @@
 #define MATRIX_H
 const int MAX_COL = 5;
 
+//Fuction Prototypes
 double sumOfRow(const double mat[][MAX_COL], const int row, const int maxRow);
 double sumOfCol(const double mat[][MAX_COL], const int column, const int maxRow);
 void fillWithRandomNum(double mat[][MAX_COL], const int maxRow);
